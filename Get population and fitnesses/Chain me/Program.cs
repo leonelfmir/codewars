b6cf1ae42a278ecabb338902e1fe5e5c2579ef73
leonel <leoenlfmir@hotmail.com>
2016-06-22 15:38:17 -0400

    2 more

---
+++ Get population and fitnesses/Chain me/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_me
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static double Chain(double input, Func<double, double>[] fs)
+        {
+            // your code ...
+            //foreach (Func<double, double> f in fs)
+            //{
+            //    input = f(input);
+            //}
+            //return input;
+
+
+            return fs.Aggregate(input, (current, t) => t(current));
+        }
+    }
+}
---
+++ Get population and fitnesses/Mombling/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mombling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "ZpglnRxqenU";
+            s = Accumul.Accum(s);
+            
+            Console.WriteLine(s);
+            Console.Read();
+        }
+    }
+
+    public class Accumul
+    {
+        public static String Accum(string s)
+        {
+            // your code
+            StringBuilder res = new StringBuilder();
+
+            int counter = 0;
+            foreach (char c in s)
+            {
+                if(counter > 0)
+                    res.Append("-");
+
+                res.Append(c.ToString().ToUpper());
+                
+                for(int i = 0; i < counter; i++)
+                {
+                    res.Append(c.ToString().ToLower());
+                }
+
+                
+                counter++;
+            }
+
+            return res.ToString();
+        }
+    }
+}
