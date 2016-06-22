82db6adbf65543871bd4bb80b6bc9b9b19492615
leonel <leoenlfmir@hotmail.com>
2016-06-22 10:19:43 -0400

    test

---
+++ Get population and fitnesses/Get population and fitnesses/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_population_and_fitnesses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public class ChromosomeWrap
+        {
+            public string Chromosome { get; set; }
+            public double Fitness { get; set; }
+        }
+        public class Kata
+        {
+            public IEnumerable<ChromosomeWrap> MapPopulationFit(IEnumerable<string> population, Func<string, double> fitness)
+            {
+                List<ChromosomeWrap> L = new List<ChromosomeWrap>();
+
+                foreach (string s in population)
+                {
+                    L.Add(new ChromosomeWrap() { Chromosome = s, Fitness = fitness(s) });
+                }
+
+                return L;
+            }
+        }
+    }
+}
